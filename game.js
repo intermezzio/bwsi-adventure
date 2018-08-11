2805f97cbb9c93f9ccb14f29958e69f73bb1f286
intermezzio <amascillaro@gmail.com>
2018-08-11 13:19:41 -0400

    neaten formatting

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
 class Game {
-	constructor(canvas) {
-		this.canvas = canvas;
-	}
-	get canvas() {
-		return this.canvas;
-	}
+	constructor(outputEl, buttonsEl, inventoryEl) {
+		this.state = 0; // game state, 0 = start
+		this.outputEl = outputEl; // DOM element
+		this.buttonsEl = buttonsEl;
+		this.inventoryEl = inventoryEl;
+
+		this.output = Array();
+		this.buttons = Array();
+		this.inventory = {
+			item: 0,
+			//items : quantity
+		};
+	};
+	
 }
