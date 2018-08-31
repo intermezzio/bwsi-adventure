e59b9d9160247aa0cd7a10e23a781d303c1c0abc
intermezzio <amascillaro@gmail.com>
2018-08-31 18:22:05 -0400

    Reformat JS Objects and make the first sample objects.

--- game.js
+++ game.js
@@ -1,17 +1,26 @@
-class Game {
-	constructor(outputEl, buttonsEl, inventoryEl) {
-		this.state = 0; // game state, 0 = start
-		this.outputEl = outputEl; // DOM element
-		this.buttonsEl = buttonsEl;
-		this.inventoryEl = inventoryEl;
+var Game = function(output, buttons, inventory) {
+	this.state = 0; // game state, 0 = start
+	this.outputEl = outputEl; // DOM element
+	this.btnsEl = buttonsEl;
+	this.inventoryEl = inventoryEl;
+	this.out = Array(); // output array
+	this.inventory = {
+		food: 5
+	}
+}
 
-		this.output = Array();
-		this.buttons = Array();
-		this.inventory = {
-			item: 0,
-			otherItem: 0
-			//items : quantity
-		};
-	};
-	
+var Output = function(text, options) {
+	this.txt = text; // text
+	this.optns = options || Array(); // btn list
+}
+
+var Popup = function(text, btns) {
+	this.txt = text; // text
+	this.btns = btns || Array(); // btn list
+}
+
+var Btn = function(text, onclick, pos) {
+	this.txt = text; // btn text
+	this.onclick = onclick; // function on click
+	this.pos = pos; // -1: left, 0: center, 1: right
 }
---
+++ objects.js
@@ -0,0 +1,8 @@
+var backgroundStory = new Popup();
+backgroundStory.txt = "You are from the city ________ in the year _______.  Your enemies, the ________, have just invaded the city.\n\nYou were running for your life as they ran through the city, and fell into a small underground shelter.\n\nYou know the ________ will invade the rest of your country if you don't warn them soon.";
+backgroundStory.btns = [
+	new Btn("Start your Journey", function(game) {
+		// what the game does on init
+		return
+	}, 0)
+];
